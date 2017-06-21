be3b93b168c2e9d9619580813d8000b6a9beafb3
cuiyunxiao <196921cyx666>
2017-06-21 18:20:55 +0800

    slider图制作

--- src/components/slideShow.vue
+++ src/components/slideShow.vue
@@ -1,26 +1,19 @@
 <template>
-  <div class="slide-show" @mouseover="clearInv" @mouseout="runInv">
-    <div class="slide-img">
-      <a :href="slides[nowIndex].href">
-        <transition name="slide-trans">
-          <img v-if="isShow" :src="slides[nowIndex].src">
-        </transition>
-        <transition name="slide-trans-old">
-          <img v-if="!isShow" :src="slides[nowIndex].src">
-        </transition>
-      </a>
-    </div>
-    <h2>{{ slides[nowIndex].title }}</h2>
-    <ul class="slide-pages">
-      <li @click="goto(prevIndex)">&lt;</li>
-      <li v-for="(item, index) in slides"
-      @click="goto(index)"
-      >
-        <a :class="{on: index === nowIndex}">{{ index + 1 }}</a>
-      </li>
-      <li @click="goto(nextIndex)">&gt;</li>
-    </ul>
-  </div>
+ <div  class="slide-show">
+ <div class="slide-img">
+  <a :href="slides[nowIndex].href"> 
+    <img :src="slides[nowIndex].src">
+  </a>
+ </div>
+ <h2>{{slides[nowIndex].title}}</h2>
+ <ul class="slide-pages">
+    <li>&lt;</li>
+    <li v-for="(item, index) in slides" @click="goto(index)">
+      <a>{{ index + 1}}</a>    
+    </li>
+    <li>&gt;</li>
+  </ul>
+</div>
 </template>
 
 <script>
